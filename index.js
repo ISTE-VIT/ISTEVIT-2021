59a9c2629b94004d430efb00be77c2a89982428a
Sahil Kaling <sahil@example.com>
2021-07-31 22:42:39 +0530

    project initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+app.use(express.json({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+  
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
