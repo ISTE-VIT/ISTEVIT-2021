5f49188ed7bd027bb97f1396b1811be6a5205550
sahildotexe <sahilkaling@gmail.com>
2021-11-13 00:27:38 +0530

    messages done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const webinarRouter = require('./routes/webinar')
 
 
@@ -15,6 +17,18 @@ app.set('view engine', 'ejs');
 app.use('/',webinarRouter)
   
 const port = process.env.PORT || 3000;
+
+const db = process.env.MONGO_URI;
+//ConnectDb
+
+const connect = mongoose
+    .connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log("Mondo db connected...."))
+    .catch((err) => console.log(err));
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
